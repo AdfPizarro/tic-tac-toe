347a0ba6e84d9fefe4f544de35bf44b95e75c2c8
Yahir Cardona <yahir.cardona@gmail.com>
2020-06-23 14:40:58 -0600

    User interface created Board is displayed according user inputs

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,36 @@
 #!/usr/bin/env ruby
-puts 'Hello world'
+class Board
+    attr_accessor :board
+    def initialize (arr)
+        self.board = arr.to_a
+    end
+
+def set_board (player, position)
+    if player == 1
+        self.board[position-1] = "X"
+    else
+        self.board[position-1] = "O"
+    end
+    puts "|#{self.board[6]}||#{self.board[7]}||#{self.board[8]}|"
+    puts "|#{self.board[3]}||#{self.board[4]}||#{self.board[5]}|"
+    puts "|#{self.board[0]}||#{self.board[1]}||#{self.board[2]}|"
+
+end
+
+end
+
+board = Board.new((1..9))
+
+
+
+puts 'player 1: Choose the position for the X'
+
+print board.set_board(1, gets.chomp.to_i)
+
+puts 'player 2: Choose the position for O'
+
+print board.set_board(2, gets.chomp.to_i)
+
+
+
+
