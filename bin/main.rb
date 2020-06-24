9fec0d4de27f5336e69de9ec8678d3eb4bb6c079
Yahir Cardona <yahir.cardona@gmail.com>
2020-06-24 10:10:03 -0600

    fixed linters errors

--- bin/main.rb
+++ bin/main.rb
@@ -13,29 +13,24 @@ while continue
   puts "|#{board[0]}||#{board[1]}||#{board[2]}|"
   player = 1
   while game
-   
     until valid_value
-      if player.odd?
-        puts 'player 1: Choose the position for the X'
-      else
-        puts 'player 2: Choose the position for the ●'
-      end
+      puts "player #{player}: Choose the position for the X"
       gets.chomp.to_i
-      puts "invalid value" if valid_value == true
+      puts 'invalid value'
       puts "|#{board[6]}||#{board[7]}||#{board[8]}|"
       puts "|#{board[3]}||#{board[4]}||#{board[5]}|"
       puts "|#{board[0]}||#{board[1]}||#{board[2]}|"
       valid_value = true
     end
-    player += 1
-    puts "checking if the game ends"
+    player = player == 1 ? 2 : 1
+    puts 'checking if the game ends'
     game = false
     continue = false
   end
-  
-  puts "player 1 has win x times"
-  puts "player 2 has win x times"
-  puts "Draws x times"
+
+  puts 'player 1 has win x times'
+  puts 'player 2 has win x times'
+  puts 'Draws x times'
   puts 'Do you want to continue Y/N'
   continue = gets.chomp.upcase == 'Y'
   game = true
