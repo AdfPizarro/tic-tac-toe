f6a23cd3509c13cc23c1dbf775a1381ae22db389
Adrian <adfpizarro@gmail.com>
2020-06-29 13:24:24 -0600

    Added test to all classes, updated readme
    Included the instreuctions to run the rspec tests in the readme file
    Added the screenshot
    Splitted the test into 3 files one for each class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,34 +1,4 @@
-# spec/tictac_spec.rb
 require_relative '../lib/board.rb'
-require_relative '../lib/logic.rb'
-require_relative '../lib/player.rb'
-require_relative '../lib/scores.rb'
-
-describe Player do
-  it 'Check inint player 1' do
-    player = Player.new
-    expect(player.player).to eql(1)
-  end
-
-  it 'Check switch player 1 to 2' do
-    player = Player.new
-    board = Board.new(1..9)
-    player.move(1, board)
-    expect(player.player).to eql(2)
-  end
-
-  it 'Check inint player 1 symbol ' do
-    player = Player.new
-    expect(player.symbol).to eql('X')
-  end
-
-  it 'Check switch player 1 to 2 symbol' do
-    player = Player.new
-    board = Board.new(1..9)
-    player.move(1, board)
-    expect(player.symbol).to eql('●')
-  end
-end
 
 describe Board do
   it 'Check valid movement pos 1' do
