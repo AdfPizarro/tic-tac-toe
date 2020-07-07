43aaadd52b63160efbaa0aa506b54c31ce50c6b6
Emas <emas.dev@gmail.com>
2020-07-07 17:37:22 -0500

    Add tests for win and draw combinations

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,17 +12,49 @@ describe Board do
   end
 
   it 'return a valid position' do
-  board = Board.new
-  actual = board.set_board(1, 10)
-  expected = true
-  expect(actual).to eq expected
-end
+    board = Board.new
+    actual = board.set_board(1, 9)
+    expected = true
+    expect(actual).to eq expected
+  end
 
+  it 'return win if there are is a winning combination' do
+    board = Board.new
+    board.set_board(1,7)
+    board.set_board(2,8)
+    board.set_board(1,9)
+    board.set_board(2,4)
+    board.set_board(1,5)
+    board.set_board(2,6)
+    board.set_board(1,1)
+    board.set_board(2,2)
+    board.set_board(1,3)
+    board.check_board
+    expected = 'win'
+    expect(board.game_status).to eq expected
+  end
 
+  it 'return draw if there are is no winning combination' do
+    board = Board.new
+    board.set_board(1,1)
+    board.set_board(2,2)
+    board.set_board(1,3)
+    board.set_board(1,4)
+    board.set_board(2,5)
+    board.set_board(1,6)
+    board.set_board(2,7)
+    board.set_board(1,8)
+    board.set_board(2,9)
+    board.check_board
+    expected = 'draw'
+    expect(board.game_status).to eq expected
+  end
 
-
-
-
+  it 'return the reset board' do
+    board = Board.new
+    expect(board.board).to eq (1..9).to_a
+    expect(board.game_status).to eq ''
+  end
 end
 
 
