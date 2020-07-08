ecd2cd50a1c796b286c0d057ff1eb0808803ce9b
Emas <emas.dev@gmail.com>
2020-07-08 11:57:05 -0500

    Add tests for player and rubocop offenses fixed

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,7 +7,6 @@ describe Board do
     expected = "|7|8|9|\n"
     expected += "|4|5|6|\n"
     expected += '|1|2|3|'
-    expected
     expect(actual).to eq expected
   end
 
@@ -20,15 +19,15 @@ describe Board do
 
   it 'return win if there are is a winning combination' do
     board = Board.new
-    board.set_board(1,7)
-    board.set_board(2,8)
-    board.set_board(1,9)
-    board.set_board(2,4)
-    board.set_board(1,5)
-    board.set_board(2,6)
-    board.set_board(1,1)
-    board.set_board(2,2)
-    board.set_board(1,3)
+    board.set_board(1, 7)
+    board.set_board(2, 8)
+    board.set_board(1, 9)
+    board.set_board(2, 4)
+    board.set_board(1, 5)
+    board.set_board(2, 6)
+    board.set_board(1, 1)
+    board.set_board(2, 2)
+    board.set_board(1, 3)
     board.check_board
     expected = 'win'
     expect(board.game_status).to eq expected
@@ -36,15 +35,15 @@ describe Board do
 
   it 'return draw if there are is no winning combination' do
     board = Board.new
-    board.set_board(1,1)
-    board.set_board(2,2)
-    board.set_board(1,3)
-    board.set_board(1,4)
-    board.set_board(2,5)
-    board.set_board(1,6)
-    board.set_board(2,7)
-    board.set_board(1,8)
-    board.set_board(2,9)
+    board.set_board(1, 1)
+    board.set_board(2, 2)
+    board.set_board(1, 3)
+    board.set_board(1, 4)
+    board.set_board(2, 5)
+    board.set_board(1, 6)
+    board.set_board(2, 7)
+    board.set_board(1, 8)
+    board.set_board(2, 9)
     board.check_board
     expected = 'draw'
     expect(board.game_status).to eq expected
@@ -56,5 +55,3 @@ describe Board do
     expect(board.game_status).to eq ''
   end
 end
-
-
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,13 +1,31 @@
 require './lib/player'
+require './lib/board'
 
 describe Player do
-  it 'switch the player and symbol' do
+  it 'switch player 1' do
     player = Player.new
     player.switch_player
     expect(player.player).to eq 1
     expect(player.symbol).to eq 'X'
   end
 
-  it 'makes a move' do
-    player = Player
+  it 'switch the player 2' do
+    player = Player.new
+    player.switch_player
+    player.switch_player
+    expect(player.player).to eq 2
+    expect(player.symbol).to eq '●'
+  end
+
+  it 'player makes a valid move' do
+    player = Player.new
+    board = Board.new
+    expect(player.move(1, board)).to eq true
+  end
+
+  it 'player makes a invalid move' do
+    player = Player.new
+    board = Board.new
+    expect(player.move(10, board)).to eq false
+  end
 end
